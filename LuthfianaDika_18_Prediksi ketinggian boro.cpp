1a6cb167b8c9f5101490bf0203288db41f7d49b1
Luthfiana Dika <141222002+luthfianadika@users.noreply.github.com>
2023-10-10 09:56:47 +0700

    Add files via upload

---
+++ LuthfianaDika_18_Prediksi ketinggian boro.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    int s;
+    int v;
+    double t;
+    int status;
+    double sinA;
+	
+	cout << "Masukkan Nilai Sudut adalah: ";
+    cin >> s;
+    cout << "Masukkan Nilai kecepatan adalah: ";
+    cin >> v;
+    cout << "Masukkan Nilai Waktu adalah: ";
+    cin >> t;
+    sinA = sin((double) (s * 22) / 7 / 180);
+    t = pow(v, 2) * pow(sinA, 2) / 20;
+    cout << t << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
